81c778b079d8ed885ae1ca0fd2a735ac782f4c94
Kellbis <holasoykellbis@gmail.com>
2025-01-19 11:20:45 -0400

    typing useState

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Random_Fox</title>
+      </head>
       <body className="min-h-screen flex flex-col">
         <main className="flex-grow p-4">
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
 
 const random = (): number => {
@@ -6,18 +8,23 @@ const random = (): number => {
 }
 
 const Home = (): React.JSX.Element => {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`
+  ])
   return (
-    <div>
+    <div> 
+      <h1 className="text-3xl font-bold underline text-violet-500">Hello world!</h1>
+      {images.map((image, index) => (
+        <div key={index} className="p-4">
+  <RandomFox image={ image} />
+        </div>
+
+      ))}
     
-       <h1 className="text-3xl font-bold underline text-violet-500">
-    Hello world!
-      </h1>
       
-      <RandomFox image={ `https://randomfox.ca/images/${random()}.jpg`} />
-      
-      <footer >
-        
-      </footer>
     </div>
   );
 }
