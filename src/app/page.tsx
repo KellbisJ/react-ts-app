c74e908f20016b1f9758953aa639d52b8ab4fa19
Kellbis <holasoykellbis@gmail.com>
2025-01-20 08:59:39 -0400

    useState with personalized types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,30 @@
 
 import React, { useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
+import { log } from "console";
+
+type ImageFox = { id: string, url: string}
 
 const random = (): number => {
   return Math.floor(Math.random() * 123 + 1)
 }
 
 const Home = (): React.JSX.Element => {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`
+  const [images, setImages] = useState<Array<ImageFox>>([
+    {id: crypto.randomUUID(), url: `https://randomfox.ca/images/${random()}.jpg`},
+     {id: crypto.randomUUID(), url: `https://randomfox.ca/images/${random()}.jpg`},
+     {id: crypto.randomUUID(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id: crypto.randomUUID(), url: `https://randomfox.ca/images/${random()}.jpg`}
   ])
+
+  console.log(images);
+  
   return (
     <div> 
       <h1 className="text-3xl font-bold underline text-violet-500">Hello world!</h1>
-      {images.map((image, index) => (
-        <div key={index} className="p-4">
-  <RandomFox image={ image} />
+      {images.map(({id, url}) => (
+        <div key={id} className="p-4">
+  <RandomFox image={ url} />
         </div>
 
       ))}
