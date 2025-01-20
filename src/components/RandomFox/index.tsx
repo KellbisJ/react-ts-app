4f6babc893046a6ec04a8a743a66feb4e2af02c9
Kellbis <holasoykellbis@gmail.com>
2025-01-20 10:28:25 -0400

    lazy loading with observers

--- src/components/RandomFox/index.tsx
+++ src/components/RandomFox/index.tsx
@@ -1,14 +1,35 @@
-import React, {useRef} from "react"
+import React, { useRef, useEffect, useState } from "react"
 
 type Props = {
   image: string;
 }
 
 export const RandomFox = ({ image }: Props): React.JSX.Element => {
-  const node = useRef<HTMLImageElement>(null)
+  const [imgSrc, setImgSrc] = useState<string>("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=")
 
+  const node = useRef<HTMLImageElement>(null)
+  
+  useEffect(() => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+         setImgSrc(image)
+         
+       }
+    })
+  })
+
+    if (node.current) {
+      observer.observe(node.current)
+    }
+    
+    return () => {
+      observer.disconnect()
+    }
+  }, [image])
+  
 
   return (
-    <img ref={node} src={image} className="w-[320px] h-auto rounded" />
+    <img ref={node} src={imgSrc} className="w-[320px] h-auto rounded bg-gray-300" />
 )  
 }
