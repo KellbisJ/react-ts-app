fd9aec329946b87f2cbeb5c26f4e9b58662db48a
Kellbis <holasoykellbis@gmail.com>
2025-01-20 12:11:32 -0400

    challenge completed

--- src/components/RandomFox/index.tsx
+++ src/components/RandomFox/index.tsx
@@ -2,32 +2,37 @@ import React, { useRef, useEffect, useState } from "react"
 import { ImgHTMLAttributes } from "react"
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
-  src: string
+  src: string,
+  onLazyLoad?: (imgElement: HTMLImageElement) => void
 }
 
-export const LazyImage = ({ src, ...props }: Props): React.JSX.Element => {
+export const LazyImage = ({ src, onLazyLoad, ...props }: Props): React.JSX.Element => {
   const [imgSrc, setImgSrc] = useState<string>("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=")
+  const [lazyLoadExecuted, setLazyLoadExecuted] = useState<boolean>(false)
 
   const node = useRef<HTMLImageElement>(null)
   
   useEffect(() => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-      if (entry.isIntersecting) {
-         setImgSrc(src)
-         
+      if (entry.isIntersecting && !lazyLoadExecuted) {
+        setImgSrc(src)
+      } if (onLazyLoad && node.current) {
+        onLazyLoad(node.current)
+        setLazyLoadExecuted(true)
        }
     })
   })
 
     if (node.current) {
+
       observer.observe(node.current)
     }
     
     return () => {
       observer.disconnect()
     }
-  }, [src])
+  }, [src, onLazyLoad, lazyLoadExecuted])
   
 
   return (
