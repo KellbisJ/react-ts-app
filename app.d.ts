43e1cfd71bff47ce3ef561feeb827787ae25e73b
Kellbis <holasoykellbis@gmail.com>
2025-01-21 08:11:33 -0400

    global types

---
+++ app.d.ts
@@ -0,0 +1,1 @@
+type IFoxImage = { id: string; url: string };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,19 @@ import React, { useState } from "react";
 import { MouseEventHandler} from "react" // Use go to definition and watch how the types are defined. And then you can import those types, like in this case a type that is related with the DOM
 import { LazyImage } from "@/components/RandomFox";
 
-type ImageFox = { id: string, url: string}
 
 const random = (): number => {
   return Math.floor(Math.random() * 123 + 1)
 }
 
 const Home = (): React.JSX.Element => {
-  const [images, setImages] = useState<Array<ImageFox>>([])
+  const [images, setImages] = useState<Array<IFoxImage>>([])
 
   const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.preventDefault()
   
 
-    const newFoxImage: ImageFox = {
+    const newFoxImage: IFoxImage = {
     id: crypto.randomUUID(),
     url: `https://randomfox.ca/images/${random()}.jpg?${new Date().getTime()}`
     }
